91f0aea6ed851e7e30a61387530b6903974963d0
Thiliban <thilip2017@gmail.com>
2022-11-05 18:46:12 +0530

    first commit

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((user, i) => (
+    <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+  ));
+  return (
+    <>
+      {cardComponent}
+    </>
+  );
+};
+
+export default CardList;
